17a27dc485704978cec1ef36bfff021070524b47
Atmaram <atmaramgondage01@gmail.com>
2023-08-30 15:32:33 +0530

    this secound commit

--- SpringCoreFristProject/src/main/java/com/atm/Dbcon.java
+++ SpringCoreFristProject/src/main/java/com/atm/Dbcon.java
@@ -36,6 +36,11 @@ public class Dbcon {
 	@Override
 	public String toString() {
 		return "Dbcon [id=" + id + ", name=" + name + ", LastName=" + LastName + "]";
+
+		int i =10;
+		int j=20;
+		int k=i+j;
+		System.out.println(k);
 	}
     
 	
